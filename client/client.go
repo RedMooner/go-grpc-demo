3e871bf9fea45efe2ab41de9150bbd521da07ff1
Alexandr Motyrev <47520961+RedMooner@users.noreply.github.com>
2025-03-05 12:14:17 +0500

    Add files via upload

---
+++ client/client.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+	"google.golang.org/grpc"
+	pb "go-grpc-demo/src/go"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: "World"})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
+}
